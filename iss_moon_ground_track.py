49ad744434ca424a11e97acf1a004c8489e8f42d
tjmurphy <tjmurphy@mit.edu>
2021-10-01 14:01:12 -0400

    Got the ground track working

--- iss_moon_ground_track.py
+++ iss_moon_ground_track.py
@@ -3,6 +3,9 @@ from skyfield.constants import AU_KM
 from skyfield.positionlib import Geocentric
 from numpy.linalg import norm
 import load_tle
+import matplotlib.pyplot as plt
+import cartopy
+import cartopy.crs as ccrs
 
 def normalize(vector):
     return vector / norm(vector)
@@ -13,7 +16,7 @@ def find_earth_intersect_point(origin,vector,earth_radius):
     #Step 2a: First, extend the vector over and over until you punch through the earth.
     #Note: This step is risky if the vector is near-tangent with the earth.
     #Only use when you know the vector is coming in roughly vertical to the surface.
-    length_guess = 1
+    length_guess = 1 / AU_KM
     while(norm(origin + unit_vector_toward_ground * length_guess) > RE):
         length_guess *= 2
     #Once we find that number, it's under the earth. That's an upper bound on length.
@@ -24,7 +27,7 @@ def find_earth_intersect_point(origin,vector,earth_radius):
     #exact required length.
     error = 9999
     #Tolerance of 0.1 km should get us quite close.
-    while(abs(error) > 0.1):
+    while(abs(error) > 0.1 / AU_KM):
         #Standard binary search from here, go higher or lower based on where the guess winds up.
         new_guess = (length_lower_bound + length_upper_bound) / 2
         new_vector_end = norm(origin + unit_vector_toward_ground * new_guess)
@@ -33,39 +36,74 @@ def find_earth_intersect_point(origin,vector,earth_radius):
         else:
             length_upper_bound = new_guess
         error = new_vector_end - RE
-    print(new_guess)
     intersection_point = origin + unit_vector_toward_ground * new_guess
-    print(f"norm = {norm(intersection_point)}")
-    print(type(intersection_point[0]))
-    print(norm(intersection_point))
-    geo = Geocentric(intersection_point / AU_KM,t=t)
-    print(geo.distance().km)
-    subpoint = wgs84.subpoint(geo)
-    print(subpoint.elevation.km)
 
+    geo = Geocentric(intersection_point,t=t)
+    subpoint = wgs84.subpoint(geo)
     return subpoint.latitude.degrees,subpoint.longitude.degrees
 
 ts = load.timescale()
 
-t = ts.utc(2021,9,23,4,31,59)
+t = ts.utc(2021,10,3,22,51,range(30,151))
 
-CAMBRIDGE = 42.371539,-71.098857, 0
-ODDSITE = 40.33825356986421,-72.86106736111111,0
 DURANGO = 37.273267,-107.871692, 2000
+LOS_ANGELES = 34.0,-118.2, 100
+BOULDER = 40.015, -105.270556,1655
+SEATTLE = 47.609722, -122.333056, 100
+CAMBRIDGE = 42.371539,-71.098857, 20
+NYC = 40.712778, -74.006111,20
 
 
-RE = norm(wgs84.latlon(*DURANGO).at(t).position.km)
+RE = norm(wgs84.latlon(*SEATTLE).at(t).position.au)
 planets = load('de421.bsp')
 earth = planets['earth']
 moon = planets['moon']
 
-sat_tle = load_tle.get_tle(25544)
+#sat_tle = load_tle.get_tle(25544)
+
+sat_tle = [
+"1 25544U 98067A   21273.48726090  .00001212  00000-0  30298-4 0  9993",
+"2 25544  51.6456 182.0705 0003968  45.0544 125.1915 15.48872009304915"]
 sat = EarthSatellite(*sat_tle)
 moon = moon - earth
 
 #Get vector from the moon to the satellite; extending this vector will make
 #it run into the earth.
-vec_from_moon_to_sat = (sat - moon).at(t).position.km
-lat,long = find_earth_intersect_point(sat.at(t).position.km,vec_from_moon_to_sat,RE)
-print(lat,long)
 
+vec_from_moon_to_sat = (sat - moon).at(t).position.au
+lat,long = find_earth_intersect_point(sat.at(t).position.au,vec_from_moon_to_sat,RE)
+
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.set_facecolor("black")
+ax.add_feature(cartopy.feature.COASTLINE,edgecolor='lightgreen')
+states_provinces = cartopy.feature.NaturalEarthFeature(
+    category='cultural',
+    name='admin_1_states_provinces_lines',
+    scale='110m',
+    edgecolor='lightgreen',facecolor='none')
+ax.add_feature(cartopy.feature.BORDERS,edgecolor='lightgreen')
+ax.add_feature(states_provinces)
+ax.set_extent(
+    [min(long) - 2,
+     max(long) + 2,
+     min(lat) - 2,
+     max(lat) + 2],
+    crs=ccrs.PlateCarree())
+#Plot initial location
+ax.plot(long,lat,transform=ccrs.PlateCarree())
+#Now for the good stuff: Labeling.
+long_labels = long[::10]
+lat_labels = lat[::10]
+label_times = t[::10]
+labels = zip(label_times,long_labels,lat_labels)
+for l in labels:
+    timetext = l[0].utc_strftime("%H:%M:%S")
+    ax.annotate(timetext,
+                (l[1],l[2]),
+                textcoords="offset points",
+                xytext=(5,-5),
+                color="w",
+                transform=ccrs.PlateCarree())
+ax.scatter(long_labels,lat_labels,marker=".",transform=ccrs.PlateCarree())
+
+plt.show()
